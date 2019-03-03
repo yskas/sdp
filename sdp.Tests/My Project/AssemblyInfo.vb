dd2b514d57c7959ba310cc4facb0d8bd2640cfe4
yusukeaso <ya8121.dev@gmail.com>
2019-03-04 02:15:02 +0900

    first commit

---
+++ sdp.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は、以下の属性セットによって 
+' 制御されます。アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更します。
+
+' アセンブリの属性値を確認します
+
+<Assembly: AssemblyTitle("sdp.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("sdp.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("afe8dea4-c815-4687-96f7-03ec9f3647bc")>
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
